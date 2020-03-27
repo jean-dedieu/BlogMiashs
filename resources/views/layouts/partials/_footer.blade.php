6f7869f77f49675cacd9f3316b10a3a8e56ceaae
Jean de Dieu TWAGIRUMUHOZA <jeandedieu.emploi@gmail.com>
2020-03-27 14:28:10 +0100

    mise a jour _footer

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="text-center">
+<p><b>&copy; {{date('Y')}} &middot; <b></p>
+<p><b>Blog Par Jean de Dieu TWAGIRUMUHOZA &     Abdoulaye KEITA</b> </p><br>
+<p><b>Etudiants Master 2 MIASHS <b></p><br>
+<p><b>Université Grenoble Alpes<b></p>
+<p><i class="fab fa-github"></i></p>
+<i class="fab fa-facebook"></i>
+
+<footer>

5b0cc8bcc68928866f7311da078b295cad5f7bfa
Jean de Dieu TWAGIRUMUHOZA <jeandedieu.emploi@gmail.com>
2020-03-27 14:21:22 +0100

    Creation de dossier pages qui est utilise par PagesController

---
+++ resources/views/layouts/pages/about.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.main',['title' => 'About'])
+
+
+
+@section('content')
+<div class="row"> 
+    <div class="text-center">
+      <h2> ADN D'{{ env('APP_NAME')}}</h2>
+      
+    </div>
+</div>
+
+
+<div class="container">
+
+
+
+
+<div class="row">
+          <div class="col-md-12">
+          <p>In hac habitasse platea dictumst. Donec ornare ipsum justo. Duis malesuada lacinia convallis. 
+         In quis leo eu nisl dapibus hendrerit eget a lorem. Phasellus eu metus facilisis elit placerat ullamcorper. 
+         Fusce vulputate risus ipsum, a bibendum nibh vehicula sed. Morbi a imperdiet nibh, quis convallis lorem. 
+         Proin venenatis risus vel tempus efficitur. Donec rhoncus purus ut erat aliquet, id placerat enim varius.
+         Fusce tincidunt ac neque sed porttitor. Vestibulum maximus nibh dui, nec venenatis sapien suscipit id. 
+         Nam gravida sagittis turpis, et iaculis tortor dictum et. Donec bibendum tempus ante, dictum vestibulum sem posuere sodales. Phasellus pretium sodales eros.
+    </p>
+  </div>
+    
+</div>
+<div class="row">
+          <div class="col-md-12">
+          <p>In hac habitasse platea dictumst. Donec ornare ipsum justo. Duis malesuada lacinia convallis. 
+         In quis leo eu nisl dapibus hendrerit eget a lorem. Phasellus eu metus facilisis elit placerat ullamcorper. 
+         Fusce vulputate risus ipsum, a bibendum nibh vehicula sed. Morbi a imperdiet nibh, quis convallis lorem. 
+         Proin venenatis risus vel tempus efficitur. Donec rhoncus purus ut erat aliquet, id placerat enim varius.
+         Fusce tincidunt ac neque sed porttitor. Vestibulum maximus nibh dui, nec venenatis sapien suscipit id. 
+         Nam gravida sagittis turpis, et iaculis tortor dictum et. Donec bibendum tempus ante, dictum vestibulum sem posuere sodales. Phasellus pretium sodales eros.
+    </p>
+  </div>  
+</div>
+<div class="row">
+          <div class="col-md-12">
+          <p>In hac habitasse platea dictumst. Donec ornare ipsum justo. Duis malesuada lacinia convallis. 
+         In quis leo eu nisl dapibus hendrerit eget a lorem. Phasellus eu metus facilisis elit placerat ullamcorper. 
+         Fusce vulputate risus ipsum, a bibendum nibh vehicula sed. Morbi a imperdiet nibh, quis convallis lorem. 
+         Proin venenatis risus vel tempus efficitur. Donec rhoncus purus ut erat aliquet, id placerat enim varius.
+         Fusce tincidunt ac neque sed porttitor. Vestibulum maximus nibh dui, nec venenatis sapien suscipit id. 
+         Nam gravida sagittis turpis, et iaculis tortor dictum et. Donec bibendum tempus ante, dictum vestibulum sem posuere sodales. Phasellus pretium sodales eros.
+    </p>
+  </div>
+    
+</div>
+
+</div>
+
+@stop

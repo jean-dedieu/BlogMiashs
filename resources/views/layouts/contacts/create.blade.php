7efb92f6e41e3c7de479928c11ae81c7f70aaa02
Jean de Dieu  TWAGIRUMUHOZA <jeandedieutwagirumuhoza@iMac-de-Jean.local>
2020-03-29 03:23:55 +0200

    ajout de la path contact_path

--- resources/views/layouts/contacts/create.blade.php
+++ resources/views/layouts/contacts/create.blade.php
@@ -12,29 +12,31 @@
        <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1>">
            <h2>Restons en contact <i class="far fa-smile-wink"></i></h2><br>
 
-           <form action="#" method="POST">
+           <form action="{{ route('contact_path')}}" method="POST">
+                  {{ csrf_field()}}
 
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
 
                      <label for="name" class="control-label">Nom</label><br>
                        <input type="text" name="name" id="name" class="form-control" required="required" >
-
+                       {!! $errors->first('name', '<span class="help-block">:message</span>')!!}
                 </div>
                     <div class="form-group">
 
                        <label for="email" class="control-label">E-mail</label><br>
                        <input type="email" name="email" id="email" class="form-control" required="required" >
-
+                       {!! $errors->first('email', '<span class="help-block">:email</span>')!!}
                    </div>
                       <div class="form-group">
 
                        <label for="message" class="control-label" sr-only>Votre message</label><br>
                        <input type="textarea" class="form-control" rows="20" cols="30" name="message" id="message" required="required" >
+                       {!! $errors->first('message', '<span class="help-block">:message</span>')!!}
 
                    </div>
                    <div class="form-group">
 
-                       <button class="btn btn-success btn-block">J'envoi</button>
+                       <button class="btn btn-success btn-block &raquo;">J'envoi</button>
 
                       </div>
 

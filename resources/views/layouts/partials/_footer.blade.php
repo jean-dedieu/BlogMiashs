d47957283afbfeded934e205f61502ffaae3d1f7
Abdoulaye Keita <abdoulaye@hotmail.fr>
2020-03-29 14:52:28 +0200

    mise a jour footer

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="text-center">
 <p><b>&copy; {{date('Y')}} &middot; <b></p>
 <p><b>Blog Par Jean de Dieu TWAGIRUMUHOZA &     Abdoulaye KEITA</b> </p><br>
-<p><b>Etudiants Master 2 MIASHS <b></p><br>
+<p><b>Etudiants Master 2 MIASHS DCISSRS<b></p><br>
 <p><b>Université Grenoble Alpes<b></p>
 <p><i class="fab fa-github"></i></p>
 <i class="fab fa-facebook"></i>

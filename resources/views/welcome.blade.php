f643462860c44ccbe135d125350c31d80e932dbd
Abdoulaye Keita <abdoulaye@hotmail.fr>
2020-03-29 21:57:38 +0200

    modification de footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,19 @@
 
 @section('content')
 
-    <h1>Home</h1>
+    <h1>Bienvenue sur le site de Miashs</h1>
 
     <ul>
-    
+
          @foreach ( $posts as $post )
-    
+
        <li> <a  href="{{ url('http://127.0.0.1:8000/articles/'.$post->post_name) }}" >{{$post->post_title}} </a>
-    
+
 
        </li>
-      
+
          @endforeach
     </ul>
+
 
 @endsection

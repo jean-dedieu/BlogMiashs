ff05d1628f097fab8908facf5d6f39c2520db8d5
Jean de Dieu TWAGIRUMUHOZA <jeandedieu.emploi@gmail.com>
2020-03-30 22:10:17 +0200

    creation des migration pour table Message

---
+++ database/migrations/2020_03_30_190520_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //nous créons des attribut pour notre table Message qui contiendra des messages envoy"s par des utilsateurs
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->text('message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}

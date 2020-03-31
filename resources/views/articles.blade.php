1dd4c85b5a920beaf3d2aac448c874515d4b6f99
Abdoulaye Keita <abdoulaye@hotmail.fr>
2020-04-01 01:21:35 +0200

    essai avec boostrap

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -12,18 +12,18 @@
 
 <div class="class=d-flex p-2">
 <ul>
- 
-    <h1 class="text-center">A la une</h1>
+
+    <h1 class= "text-center text-light bg-dark">A la une</h1>
         @foreach ( $posts as $post )
 
-      
-        
-        <li class="list-group-item list-group-item-primary"> <a href="{{ url('http://127.0.0.1:8000/articles/'.$post->post_name) }}" >{{$post->post_title}}</a>
-    
+
+
+        <li class="list-group-item list-group-item-primary "> <a href="{{ url('http://127.0.0.1:8000/articles/'.$post->post_name) }}" >{{$post->post_title}}</a>
+
         </li>
-     
+
         @endforeach
-    
+
 </ul>
 </div>
 @endsection

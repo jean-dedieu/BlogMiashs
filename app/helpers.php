3384e043aaaf30f1bdf0f1931b9ed95e98d2b34b
Jean de Dieu TWAGIRUMUHOZA <jeandedieu.emploi@gmail.com>
2020-03-31 03:08:51 +0200

    ajout de fonction set_active_route dans app\helper.php pour reperer la la route de la page active et mise à jous des classes des li de _nav

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,5 @@
 <?php
-/*fonction helper qui nous permettra re recupérer à chaque fois le titre dans une page pour 
- setter la page à active
+/*fonction helper qui nous permettra re recupérer à chaque fois le titre dans une page 
  */
 
 if(!function_exists('page_title')){
@@ -13,5 +12,16 @@ if(!function_exists('page_title')){
         } else{
             return $title . ' | ' . $base_title; //si la page ne contient pas de titre, on affichera choMiashs - Blog dans l'onglet de browser 
         }
+    }
+}
+
+/*cette finction nous permettra de désigner la page courante dans le menu de naviagtion pour l'UX Design
+ setter la page à active */
+
+if(!function_exists('set_active_route')){
+    function set_active_route($route){
+
+        return Route::is($route) ? 'active' : '';
+       
     }
 }
--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -14,12 +14,12 @@
 
         <div id="navbar" class="navbar-collapse collapse">
          <ul class="nav navbar-nav">
-            <li><a href="{{ route('root_path') }}">Accueil</a></li>
-            <li><a href="{{ route('about_path') }}">Qui sommes-nous</a></li>
-            <li><a href="{{ route('contact_path')}}">Contacts</a></li>
+            <li class="{{ set_active_route('root_path') }}"><a href="{{ route('root_path') }}">Accueil</a></li>
+            <li class="{{ set_active_route('about_path') }}"><a href="{{ route('about_path') }}">Qui sommes-nous</a></li>
+            <li class="{{ set_active_route('contact_path') }}"><a href="{{ route('contact_path')}}">Contacts</a></li>
             <!--<li class="menu-text">Le Blog</li>-->
            <!-- <li><a href="http://127.0.0.1:8000/">Home</a></li>-->
-            <li><a href="http://127.0.0.1:8000/articles">Articles</a></li>
+            <li class="{{ set_active_route('articles_path') }}"><a href="http://127.0.0.1:8000/articles">Articles</a></li>
             <!--<li><a href="http://127.0.0.1:8000/contact">Contact</a></li>-->
 
 

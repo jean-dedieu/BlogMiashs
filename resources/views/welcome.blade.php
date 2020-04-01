341cae69257dbd5af1f1f3645703d714b621d68f
Jean de Dieu TWAGIRUMUHOZA <jeandedieu.emploi@gmail.com>
2020-04-01 13:10:21 +0200

    ajout des asides dans  la page d'acceuil, ajout du lorem ipsum dans les aside, ajout de l'image BANNER pour les slides et misen page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,19 +2,66 @@
 
 @section('content')
 
-    <h1>Bienvenue sur le site de Miashs</h1>
 
-    <ul>
+<div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1>">
+
+<h1 class="text-center bg-primary text-white">Bienvenue sur le site de Miashs</h1>
+
+</div>
+
+<img height="300" width="1500" src="app/public/storage/images/image1.jpg" alt="SLIDE DES  IMAGES "/>
+
+<div class="col-md-8 col-md-offset-4 col-sm-10 col-sm-offset-1>">
+
+<h2  class="btn btn-danger center">&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp &nbsp&nbsp &nbsp&nbsp &nbsp&nbsp  Vous trouverez ici les 3 derniers articles  publiés&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp &nbsp&nbsp &nbsp&nbsp &nbsp&nbsp</h2>
+
+</div><br><br><br><br><br>
+
+
+
+<div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1>">
+
+
+
+
+    <div class="text-center">
 
          @foreach ( $posts as $post )
 
-       <li> <a  href="{{ url('http://127.0.0.1:8000/articles/'.$post->post_name) }}" >{{$post->post_title}} </a>
+       <h3><a  href="{{ url('http://127.0.0.1:8000/articles/'.$post->post_name) }}" >{{$post->post_title}} </a><h3>
 
 
-       </li>
+       
 
          @endforeach
-    </ul>
+</div>
+<div class="row">
+  <div class="col-sm-2">
+    <div class="row">
+      <aside class="border-1"><p>
+        
+      Lorem ipsum dolor sit amet, rdum leo.
 
+Ut inib
+
+      </p></aside>
+      <aside class="border-1"><p> Lorem ipsum dolor sit amet, rdum leo.Lorem ipsum dolor sit amet, rdum leo.
+
+      </p></aside>
+    </div>
+  </div>
+  <section class="col-sm-10 col-md-8">Section</section>
+  <div class="clearfix visible-sm"></div>
+  <div class="col-md-2">	
+
+    <div class="row">
+      <aside class="col-md-12"><p>Lorem ipsum dolor sit amet,  quam nibh fringilla lacus, sit amet fermentum ligula massa et justo.</p></aside>
+      <aside class="col-md-12"><p>Sit amet fermentum ligula massa et justo. massa et justo.</p></aside>
+    </div>
+  </div>
+</div> 
+
+</div><br><br><br><br><br><br>
+    
 
 @endsection

861d84ca2401cf537370cf424e3cb9fb4b636fdb
Jean de Dieu TWAGIRUMUHOZA <jeandedieu.emploi@gmail.com>
2020-04-01 13:11:21 +0200

    mis en page de l'affichage d'un article

--- resources/views/posts/single.blade.php
+++ resources/views/posts/single.blade.php
@@ -4,14 +4,14 @@
 
 
 
-<h4 class="text-center"> Auteur : {{$user->name}} </h4>
+<h4 class="text-center"><strong> Auteur : {{$user->name}} </strong></h4><br><br><br>
+
+<div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1>">
+
+<p class="text-left">{{$post->post_content}}</p>
 
 
+</d>
 
-
-<p class="text-center">{{$post->post_content}}</p>
-
-
-<p class="text-center">Fin article</p>
 
 @endsection

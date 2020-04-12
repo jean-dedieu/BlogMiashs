2fa3b54ea7c7122fb5a8d0be5eee76616f0873e1
Jean de Dieu  TWAGIRUMUHOZA <jeandedieutwagirumuhoza@iMac-de-Jean.local>
2020-04-12 18:07:30 +0200

    mise à jour page accueil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@ $title='Home';
     <title>slide</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="jQuery Responsive Carousel - Owl Carusel">
-    <meta name="author" content="Bartosz Wojciechowski">
+   
 
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600,700' rel='stylesheet' type='text/css'>
     <link href="css/bootstrapTheme.css" rel="stylesheet">

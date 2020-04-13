221575c3367db28d0b384b818b4a2957f5f00258
Jean de Dieu  TWAGIRUMUHOZA <jeandedieutwagirumuhoza@iMac-de-Jean.local>
2020-04-13 07:54:54 +0200

    j'enlève le route de tes que j'avais mis dans web.php qui causait le conflit de merge master-menu

--- routes/web.php
+++ routes/web.php
@@ -88,13 +88,12 @@ Route::get('/home', function () {
 
 Auth::routes();
 
-<<<<<<< HEAD
+
 //Route::get('/home', 'HomeController@index');
 //->name('home');
-=======
 //Route::get('/home', 'HomeController@index')->name('home');
-
+/*
 Route::get('/home', function () {
     return view('home');
-});
->>>>>>> menu
+});*/
+

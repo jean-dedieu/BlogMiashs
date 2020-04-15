d0bcc294f3818e36d9c99c962a7a5ada29590221
Jean de Dieu  TWAGIRUMUHOZA <jeandedieutwagirumuhoza@iMac-de-Jean.local>
2020-04-15 02:19:01 +0200

    routes users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 
+<<<<<<< HEAD
 @section('content')
+=======
+
+<div class="text-center">
+>>>>>>> 74994290f102f370b0469019a1d55e193702be47
 
 
+<<<<<<< HEAD
 @endsection
+=======
+<h3><a  href="{{ url('http://127.0.0.1:8000/articles/'.$post->post_name) }}" >{{$post->post_title}} </a><h3>
+
+@endforeach
+</div>
+
+@extends('views.welcome')
+
+
+
+
+>>>>>>> 74994290f102f370b0469019a1d55e193702be47
